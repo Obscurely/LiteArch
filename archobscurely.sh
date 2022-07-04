c8a9ec5f2df8119179e8143636f358d79250ef67
Obscurely <adrian.obscurely@protonmail.com>
2022-07-04 21:51:01 +0300

    fix

--- archobscurely.sh
+++ archobscurely.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bash 0-preinstall.sh
-cp -r /root/LiteArch/mnt/root
+cp -r /root/LiteArch/mnt/root /mnt/root
 arch-chroot /mnt /root/LiteArch/1-setup.sh
 source /mnt/root/LiteArch/install.conf
 arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/LiteArch/2-user.sh
